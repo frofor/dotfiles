7d5553b2060f4ea4bdc193025fbf14ab21baed11
frofor <18nraywczifc@protonmail.com>
2025-10-02 19:03:30 +0200

    feat(nvim): add git quick fix bindings

--- .config/nvim/lua/config/git.lua
+++ .config/nvim/lua/config/git.lua
@@ -5,11 +5,16 @@ local function map(lhs, rhs)
 end
 
 local gitsigns = require('gitsigns')
-gitsigns.setup({})
+gitsigns.setup({
+	 current_line_blame = true,
+})
 
-map('<leader>gd', gitsigns.diffthis)
-map('<leader>gb', gitsigns.blame_line)
-map('<leader>gB', gitsigns.blame)
-map('<leader>go', gitsigns.preview_hunk)
 map('<leader>g[', function() gitsigns.nav_hunk('prev') end)
 map('<leader>g]', function() gitsigns.nav_hunk('next') end)
+map('<leader>gd', gitsigns.diffthis)
+map('<leader>gw', gitsigns.toggle_word_diff)
+map('<leader>gb', gitsigns.blame_line)
+map('<leader>gB', gitsigns.blame)
+map('<leader>gp', gitsigns.preview_hunk_inline)
+map('<leader>gq', gitsigns.setqflist)
+map('<leader>gQ', function() gitsigns.setqflist('all') end)
