39e107a4898ac957661b0b71b911d759711c7617
frofor <18nraywczifc@protonmail.com>
2025-10-05 14:19:18 +0200

    fix(mpv): handle missing artist or title

--- .config/mpv/scripts/track_change_notifier.lua
+++ .config/mpv/scripts/track_change_notifier.lua
@@ -1,31 +1,26 @@
 local NOTIFICATION_ID = 92796
 local IMAGE_PATH = '$XDG_CONFIG_HOME/mpv/images/vinyl.png'
 
-local function escape_quotes(s)
-	return "'"..string.gsub(s, "'", '\'"\'"\'').."'"
-end
-
-local function notify(summary, text)
+---@param head string
+---@param body string
+local function notify(head, body)
 	os.execute(('notify-send -r %d -a mpv -i "%s" -- %s %s'):format(
 		NOTIFICATION_ID,
 		IMAGE_PATH,
-		escape_quotes(summary),
-		escape_quotes(text)
+		"'" .. head:gsub("'", '\'"\'"\'') .. "'",
+		"'" .. body:gsub("'", '\'"\'"\'') .. "'"
 	))
 end
 
 mp.register_event('file-loaded', function()
-	local data = mp.get_property_native('metadata')
-	if data then
-		notify(data['artist'], data['title'])
+	local meta = mp.get_property_native('metadata')
+	if meta['artist'] and meta['title'] then
+		notify(meta['artist'], meta['title'])
 		return
 	end
 
 	local path = mp.get_property('path')
-	if path then
-		local file = path:gsub('^.-([^/]+)$', '%1')
-		file = file:gsub('%....$', '')
-		local dir = path:gsub('^.-([^/]+)/[^/]*$', '%1')
-		notify(dir, file)
-	end
+	local dir = path:gsub('^.-([^/]+)/[^/]*$', '%1')
+	local file = path:gsub('^.-([^/]+)$', '%1'):gsub('%....$', '')
+	notify(dir, file)
 end)
