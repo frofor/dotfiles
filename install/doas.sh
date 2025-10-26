d0b7081c2413b1c6a10f285a990824c2f3775077
frofor <18nraywczifc@protonmail.com>
2025-10-26 11:13:57 +0100

    chore(install): fix running `doas` unprivileged

--- install/doas.sh
+++ install/doas.sh
@@ -2,7 +2,7 @@ echo Installing doas
 sudo xbps-install -y opendoas
 
 echo Configuring doas
-doas echo permit keepenv "$USER" as root > /etc/doas.conf
+sudo echo permit keepenv "$USER" as root > /etc/doas.conf
 
 echo Removing sudo
 doas xbps-remove -Fy sudo
