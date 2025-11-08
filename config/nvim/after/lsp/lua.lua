a563b6f460139f591e33ac41950df9fa2eb35fa3
frofor <18nraywczifc@protonmail.com>
2025-11-08 17:07:50 +0100

    feat(nvim): add third party libraries to lua lsp

--- config/nvim/after/lsp/lua.lua
+++ config/nvim/after/lsp/lua.lua
@@ -5,7 +5,13 @@ return {
     root_markers = { '.luarc.json', 'stylua.toml', '.git' },
     settings = {
         Lua = {
-            workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+            workspace = {
+                library = {
+                    '${3rd}/luv/library',
+                    '${3rd}/busted/library',
+                    table.unpack(vim.api.nvim_get_runtime_file('', true)),
+                },
+            },
             diagnostics = { globals = { 'mp' } },
             telemetry = { enable = false },
         },
