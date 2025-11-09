76d23c070a6617a334834254995532f25c91bacf
frofor <18nraywczifc@protonmail.com>
2025-11-09 13:02:09 +0100

    fix(nvim): add plaintex to latex lsp

--- config/nvim/after/lsp/latex.lua
+++ config/nvim/after/lsp/latex.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
     cmd = { vim.fn.stdpath('data') .. '/frofor/lsp/latex/target/release/texlab' },
-    filetypes = { 'tex' },
+    filetypes = { 'plaintex', 'tex' },
     root_markers = { '.git' },
 }
