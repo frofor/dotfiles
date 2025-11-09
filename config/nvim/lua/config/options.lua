58da650e021c275a547a214d8d34c2741f9a3b44
frofor <18nraywczifc@protonmail.com>
2025-11-09 16:48:16 +0100

    style(nvim): use spaces for indentation

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = vim.keycode('<space>')
 vim.g.maplocalleader = vim.keycode('<space>')
 
 vim.opt.mouse = ''
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.number = true
